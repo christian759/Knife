1f32241a0b617964fa5e0da58d436b14688d22f3
CEO1 <christian4onos@outlook.com>
2025-06-11 03:55:28 +0100

    11th june: prining ascii art chec

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import "fmt"
+
+func printAsciiArt(sentences []string, textFile []string) {
+	for i, word := range sentences {
+		if word == "" {
+			if i != 0 {
+				fmt.Println()
+			}
+			continue
+		}
+		for h := 1; h < 9; h++ {
+			for i := 0; i < len(word); i++ {
+				for lineIndex, line := range textFile {
+					if lineIndex == (int(word[i])-32)*9+h {
+						fmt.Print(line)
+					}
+				}
+			}
+			fmt.Println()
+		}
+	}
+}
+
 func main() {
 }
