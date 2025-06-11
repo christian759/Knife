a16be2dee02976ed1439bcb445d53ffaaf2b0f27
CEO1 <christian4onos@outlook.com>
2025-06-11 04:19:59 +0100

    11th june: added the big intro

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
 
-func printAsciiArt(sentences []string, textFile []string) {
+var width int
+
+func getTerminalSize() (int, int, error) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	size := strings.Split(string(out), " ")
+	width, err := strconv.Atoi(strings.TrimSpace(size[1]))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	height, err := strconv.Atoi(strings.TrimSpace(size[0]))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return width, height, nil
+}
+
+func printAsciiArtAlign(sentences []string, textFile []string, position string, w int) {
 	for i, word := range sentences {
 		if word == "" {
 			if i != 0 {
@@ -10,18 +40,81 @@ func printAsciiArt(sentences []string, textFile []string) {
 			}
 			continue
 		}
+		wordCount := 1
+		for _, char := range word {
+			if char == ' ' {
+				wordCount++
+			}
+		}
+		wordLen := 0
+		for i := 0; i < len(word); i++ {
+			for lineIndex, line := range textFile {
+				if lineIndex == (int(word[i])-32)*9+2 {
+					wordLen += len(line)
+					break
+				}
+			}
+		}
+		var spacesForJustify int
+		if wordCount == 1 && position == "justify" {
+			position = "center"
+		} else if wordCount == 1 {
+			spacesForJustify = (w - wordLen) / wordCount
+		} else {
+			spacesForJustify = (w - wordLen) / (wordCount - 1)
+		}
+		spaces := w/2 - wordLen/2
 		for h := 1; h < 9; h++ {
+			if position == "center" {
+				for i := 1; i <= spaces; i++ {
+					fmt.Print(" ")
+				}
+			} else if position == "right" {
+				for i := 1; i <= spaces*2; i++ {
+					fmt.Print(" ")
+				}
+			}
 			for i := 0; i < len(word); i++ {
 				for lineIndex, line := range textFile {
 					if lineIndex == (int(word[i])-32)*9+h {
-						fmt.Print(line)
+						if position == "justify" && i != len(word)-1 && word[i] == ' ' {
+							fmt.Print(line)
+							for i := 1; i <= spacesForJustify; i++ {
+								fmt.Print(" ")
+							}
+						} else {
+							fmt.Print(line)
+						}
+						break
 					}
 				}
 			}
+			if position == "center" {
+				for i := 1; i <= spaces; i++ {
+					fmt.Print(" ")
+				}
+			} else if position == "left" {
+				for i := 1; i <= spaces*2; i++ {
+					fmt.Print(" ")
+				}
+			}
+
 			fmt.Println()
 		}
 	}
 }
 
 func main() {
+	argStr := "Go-Knife"
+	sepArgs := strings.Split(argStr, "\\n")
+
+	width, _, _ = getTerminalSize()
+
+	file, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	lines := strings.Split(string(file), "\n")
+	printAsciiArtAlign(sepArgs, lines, "center", width)
 }
