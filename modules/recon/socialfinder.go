a4a5dcc3e2d063d6a1dbd9a1ce84bc5a554b23b7
CEO1 <christian4onos@outlook.com>
2025-06-11 21:04:34 +0100

    11th june: social finder mini

--- modules/recon/socialfinder.go
+++ modules/recon/socialfinder.go
@@ -1,14 +1,32 @@
 package recon
 
-var socialsite = map[string]string{
-	"facebook": "https://web.facebook.com",
-	"twitter":  "https://x.com/",
-	"github" : "https://github.com/",
-	"tiktok" : "https://"
-	"youtube":,
-	"instagram":,
-	"twitch":,
-	"medium":,
-	"threads":,
+import (
+	"fmt"
+	"net/http"
+)
 
+var socialsite = map[string]string{
+	"facebook":  "https://web.facebook.com/",
+	"twitter":   "https://x.com/",
+	"github":    "https://github.com/",
+	"tiktok":    "https://www.tiktok.com/@",
+	"youtube":   "https://www.youtube/@",
+	"instagram": "https://www.instagram/",
+	"twitch":    "https://www.twitch.tv/",
+	"medium":    "https://medium.com/@",
+	"linkedin":  "https://www.linkedin.com/in/",
+	"threads":   "https://www.threads.com/",
+}
+
+func search_user(user string) {
+	fmt.Println("Found: ")
+	for name, site := range socialsite {
+		trialsite := site + user
+		_, err := http.Get(trialsite)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Printf("%s: %s \n", name, trialsite)
+		}
+	}
 }
