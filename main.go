0d6e589940aed1569e7dc57bad5232748da1aaea
1nthe4byss <christian4onos@outlook.com>
2025-11-17 23:55:42 +0100

    17th Nov: adding the knife heading

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
 	"knife/modules/mobile"
@@ -71,7 +74,117 @@ func (m mainModel) View() string {
 	return lipgloss.NewStyle().Margin(1, 2).Render(m.list.View())
 }
 
+// getting terminal size
+func getTerminalSize() (int, int, error) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	size := strings.Split(string(out), " ")
+	width, err := strconv.Atoi(strings.TrimSpace(size[1]))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	height, err := strconv.Atoi(strings.TrimSpace(size[0]))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return width, height, nil
+}
+
+func printAsciiArtAlign(sentences []string, textFile []string, position string, w int) {
+	for i, word := range sentences {
+		if word == "" {
+			if i != 0 {
+				fmt.Println()
+			}
+			continue
+		}
+		wordCount := 1
+		for _, char := range word {
+			if char == ' ' {
+				wordCount++
+			}
+		}
+		wordLen := 0
+		for i := 0; i < len(word); i++ {
+			for lineIndex, line := range textFile {
+				if lineIndex == (int(word[i])-32)*9+2 {
+					wordLen += len(line)
+					break
+				}
+			}
+		}
+		var spacesForJustify int
+		if wordCount == 1 && position == "justify" {
+			position = "center"
+		} else if wordCount == 1 {
+			spacesForJustify = (w - wordLen) / wordCount
+		} else {
+			spacesForJustify = (w - wordLen) / (wordCount - 1)
+		}
+		spaces := w/2 - wordLen/2
+		for h := 1; h < 9; h++ {
+			switch position {
+			case "center":
+				for i := 1; i <= spaces; i++ {
+					fmt.Print(" ")
+				}
+			case "right":
+				for i := 1; i <= spaces*2; i++ {
+					fmt.Print(" ")
+				}
+			}
+			for i := 0; i < len(word); i++ {
+				for lineIndex, line := range textFile {
+					if lineIndex == (int(word[i])-32)*9+h {
+						if position == "justify" && i != len(word)-1 && word[i] == ' ' {
+							fmt.Print(line)
+							for i := 1; i <= spacesForJustify; i++ {
+								fmt.Print(" ")
+							}
+						} else {
+							fmt.Print(line)
+						}
+						break
+					}
+				}
+			}
+			switch position {
+			case "center":
+				for i := 1; i <= spaces; i++ {
+					fmt.Print(" ")
+				}
+			case "left":
+				for i := 1; i <= spaces*2; i++ {
+					fmt.Print(" ")
+				}
+			}
+
+			fmt.Println()
+		}
+	}
+}
+
 func main() {
+	argStr := "Go-Knife"
+	sepArgs := strings.Split(argStr, "\\n")
+
+	width, _, _ := getTerminalSize()
+
+	file, err := os.ReadFile("letters.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	lines := strings.Split(string(file), "\n")
+	printAsciiArtAlign(sepArgs, lines, "left", width)
+
 	// Display title
 	fmt.Println()
 	fmt.Println(tui.RenderTitle("KNIFE - Penetration Testing Toolkit"))
